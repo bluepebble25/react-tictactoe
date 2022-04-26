83ef2b7b15f67764df16ad4abbb7f9620d16241b
bluepebble25 <bluepebble25@gmail.com>
2022-04-26 17:39:14 +0900

    Upload tic-tac-toe

---
+++ src/components/Board.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Square from './Square';
+import {calculateWinner} from '../helpers';
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    // 누군가가 승리하거나 이미 사각형에 채워진 내용이 있다면 클릭 무시
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />);
+  }
+  
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
---
+++ src/helpers.js
@@ -0,0 +1,22 @@
+export function calculateWinner(squares) {
+  // 같은 도형이 연속으로 이어지는 경우들의 배열
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for(let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    // squares[a]가 null이 아니고 세 도형이 연속으로 같다면
+    if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      // 승자 반환
+      return squares[a];
+    }
+  }
+  return null;
+}
